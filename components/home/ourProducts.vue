347155e2c684e82fdede5bfad344f17da496043a
ast2019 <ezel.kaya.tr@gmail.com>
2025-03-05 13:27:50 +0330

    Update ourProducts.vue

--- components/home/ourProducts.vue
+++ components/home/ourProducts.vue
@@ -35,29 +35,19 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
-const props = defineProps({
-  productList: {
-    type: Array,
-    required: true,
-  },
-  lang:'fa'
 
-});
 const animatedElement = ref(null);
 
 onMounted(() => {
-  if ( window.innerWidth > 769){
-    gsap.from(animatedElement.value, {
-      opacity: 0,
-      y: 50,
-      duration: 1,
-      scrollTrigger: {
-        trigger: animatedElement.value,
-        start: 'top 0%',
-        toggleActions: 'play none none reverse',
-      },
-    });
-  }
-
+  gsap.from(animatedElement.value, {
+    opacity: 0,
+    y: 50,
+    duration: 1.5,
+    scrollTrigger: {
+      trigger: animatedElement.value,
+      start: "top 80%",
+      toggleActions: "play none none reverse"
+    }
+  });
 });
 </script>
